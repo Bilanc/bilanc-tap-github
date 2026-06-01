4d0bbc2f9f87f976e0b8f37bb30b3c220f452cb4
Leyu <lambert@bilanc.co>
2026-06-01 17:26:38 +0100

    pull request ids

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -1881,21 +1881,44 @@ def get_all_collaborators(schema, repo_path, state, mdata, _start_date):
     return state
 
 
+def get_default_branch(repo_path):
+    response = authed_get("repo_info", "https://api.github.com/repos/{}".format(repo_path))
+    repo_info = response.json()
+    return repo_info.get("default_branch", "main")
+
+
+def get_commit_pull_request_ids(repo_path, sha):
+    try:
+        response = authed_get(
+            "commit_pulls",
+            "https://api.github.com/repos/{}/commits/{}/pulls".format(repo_path, sha),
+            headers={"Accept": "application/vnd.github.groot-preview+json"},
+        )
+        pulls = response.json()
+        if isinstance(pulls, list):
+            return [pr.get("id") for pr in pulls if pr.get("id") is not None]
+    except Exception:
+        pass
+    return []
+
+
 def get_all_commits(schema, repo_path, state, mdata, start_date):
     """
     https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
     """
     bookmark = get_bookmark(state, repo_path, "commits", "since", start_date)
     if bookmark:
-        query_string = "?since={}".format(bookmark)
+        query_string = "&since={}".format(bookmark)
     else:
         query_string = ""
+
+    default_branch = get_default_branch(repo_path)
 
     with metrics.record_counter("commits") as counter:
         for response in authed_get_all_pages(
             "commits",
-            "https://api.github.com/repos/{}/commits{}?per_page=100".format(
-                repo_path, query_string
+            "https://api.github.com/repos/{}/commits?sha={}&per_page=100{}".format(
+                repo_path, default_branch, query_string
             ),
         ):
             commits = response.json()
@@ -1905,6 +1928,9 @@ def get_all_commits(schema, repo_path, state, mdata, start_date):
             extraction_time = singer.utils.now()
             for commit in commits:
                 commit["_sdc_repository"] = repo_path
+                commit["pull_request_ids"] = get_commit_pull_request_ids(
+                    repo_path, commit.get("sha")
+                )
                 with singer.Transformer() as transformer:
                     rec = transformer.transform(
                         commit, schema, metadata=metadata.to_map(mdata)
